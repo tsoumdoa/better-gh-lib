03c6dae6ed14d6eaefc1f273e66ff4ab61b8e05f
tsoumdoa <8blondes.swerves@icloud.com>
2025-04-25 23:11:25 +0800

    wip

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
 import { Posts, posts } from "@/server/db/schema";
 import mockData from "../../../../public/card-mock-data.json";
+import { ensureUniqueName } from "../utilities/ensureUniqueName";
 
 export const postRouter = createTRPCRouter({
   hello: publicProcedure
@@ -14,17 +15,19 @@ export const postRouter = createTRPCRouter({
     }),
 
   seed: publicProcedure.mutation(async ({ ctx }) => {
-    // const uniqueMockData = ensureUniqueName(mockData);
+    const mockName = mockData.map((item) => item.Name);
+    const uniqueName = ensureUniqueName(mockName);
+
     const maxRetry = 3;
-    let redo: Posts[] = [];
+    const redo: Posts[] = [];
     let retry = 0;
     while (retry < maxRetry && redo.length > 0) {
       mockData.forEach(
         async (item: { Name: string; Description: string }, i: number) => {
-          const res = await ctx.db
+          /* const res = */ await ctx.db
             .insert(posts)
             .values({
-              name: item.Name.replaceAll(" ", ""),
+              name: uniqueName[i],
               description: item.Description,
             })
             .catch((err) => {
@@ -33,7 +36,10 @@ export const postRouter = createTRPCRouter({
                 "SQLITE_CONSTRAINT: SQLite error: UNIQUE constraint failed: posts.name"
               ) {
                 console.log("not unique");
-                // const newName = getUniqueName(item.Name);
+                redo.push({
+                  name: uniqueName[i],
+                  description: item.Description,
+                });
               }
             });
         }
@@ -55,11 +61,4 @@ export const postRouter = createTRPCRouter({
     const posts = await ctx.db.query.posts.findMany({ limit: 10 });
     return posts;
   }),
-
-  // getLatest: publicProcedure.query(async ({ ctx }) => {
-  // 	const post = await ctx.db.query.posts.findFirst({
-  // 		orderBy: (posts, { desc }) => [desc(posts.id)],
-  // 	});
-  // 	return post ?? null;
-  // }),
 });
