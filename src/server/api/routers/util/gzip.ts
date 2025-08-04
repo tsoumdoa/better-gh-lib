0354a25a1c8355368ac9c01eb2342cf04ffa7e01
tsoumdoa <8blondes.swerves@icloud.com>
2025-08-04 17:39:55 +0800

    type err fix arrayb

--- src/server/api/routers/util/gzip.ts
+++ src/server/api/routers/util/gzip.ts
@@ -2,7 +2,7 @@ import pako from "pako";
 
 export const compress = (data: string) => {
   const gziped = pako.gzip(data);
-  return gziped;
+  return gziped as Uint8Array<ArrayBuffer>;
 };
 
 export const decompress = (data: pako.Data) => {
