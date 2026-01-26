a16eebc32aed3b16cc949ea2fc1d4f02167acf69
tsoumdoa <8blondes.swerves@icloud.com>
2026-01-26 22:08:02 +0800

    convex:generate added

---
+++ convex/schema.ts
@@ -0,0 +1,20 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+  posts: defineTable({
+    name: v.string(),
+    description: v.optional(v.string()),
+    dateCreated: v.string(),
+    dateUpdated: v.string(),
+    bucketUrl: v.string(),
+    clerkUserId: v.optional(v.string()),
+    isPublicShared: v.boolean(),
+    publicShareExpiryDate: v.optional(v.string()),
+    tags: v.optional(v.array(v.string())),
+  })
+    .index("by_clerkUserId", ["clerkUserId"])
+    .index("by_dateUpdated", ["dateUpdated"])
+    .index("by_dateCreated", ["dateCreated"]),
+});
+
